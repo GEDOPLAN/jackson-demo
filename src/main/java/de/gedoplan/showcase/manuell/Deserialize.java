91b8c902e8231772cbec6f3d93341a0af456088a
Dominik Mathmann <dominik.mathmann@gedoplan.de>
2016-05-23 11:50:53 +0200

    manuelle Serialisierung/Deserialisierung

---
+++ src/main/java/de/gedoplan/showcase/manuell/Deserialize.java
@@ -0,0 +1,34 @@
+package de.gedoplan.showcase.manuell;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+
+/**
+ *
+ * @author Dominik Mathmann
+ */
+public class Deserialize {
+    
+    public static void main(String...args) throws IOException{
+        String jsonString="            {\n" +
+"                \"orderID\": 90023,\n" +
+"                \"orderDate\": 1463751750716,\n" +
+"                \"requiredDate\": null,\n" +
+"                \"shippedDate\": null,\n" +
+"                \"freight\": 23.3,\n" +
+"                \"shipName\": \"U-203\",\n" +
+"                \"shipAddress\": \"Stieghorster Strasse 60\",\n" +
+"                \"shipCity\": \"Bielefeld\",\n" +
+"                \"shipRegion\": \"NRW\",\n" +
+"                \"shipPostalCode\": \"33605\",\n" +
+"                \"shipCountry\": \"Deutschland\",\n" +
+"                \"shipVia\": \"DHL\"\n" +
+"            }";
+        
+        
+        ObjectMapper mapper=new ObjectMapper();
+        JsonNode tree = mapper.readTree(jsonString);
+        System.out.println(tree.get("shipAddress").asText());
+    }
+}
---
+++ src/main/java/de/gedoplan/showcase/manuell/Serialize.java
@@ -0,0 +1,20 @@
+package de.gedoplan.showcase.manuell;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.gedoplan.showcase.models.Customer;
+import de.gedoplan.showcase.models.ModelFactory;
+import java.io.IOException;
+
+/**
+ *
+ * @author Dominik Mathmann
+ */
+public class Serialize {
+
+    public static void main(String... args) throws IOException {
+        Customer customer = ModelFactory.getCustomer();
+               
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(System.out, customer);
+    }
+}
